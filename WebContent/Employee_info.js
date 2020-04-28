d513c459dc0c9fb53f68eb308f8db7a8902a214f
nihei_takuya <ta-nihei@virtualex.co.jp>
2020-04-28 19:09:32 +0900

    編集機能実装開始、html枠組み、js関数のみ、サーブレットsql文の確認が必要

--- WebContent/Employee_info.js
+++ WebContent/Employee_info.js
@@ -30,7 +30,7 @@ function executeAjax () {
 				 pwemp = pw[i];
 				 console.log('for文内'+pwemp.empName);
 
-				var edit ='<td><input type="button">編集</input></td>'
+				var edit ='<td><input type="button"value="'+pwemp.empId+'" class = "js-edit-button" onclick = "deleteEdit(this)">編集</input></td>'
 				var del= ' <td><input type="button" value="'+pwemp.empId+'" class = "js-delete-button" onclick = "deleteEmp(this)"  >削除</input></td>'
 			//???	var del= ' <td><input type="button" value="削除" class = "js-delete-button" onclick = "deleteEmp('+pwemp.empId+')  >削除</input></td>'
 
@@ -98,10 +98,36 @@ function executeAjax () {
 	});
 }//var deleteEmp = functionの最後
 
-//$(document).ready(function () {
-//	'use strict';
-//	$('.js-delete-button').click(deleteEmp);
-//});
+
+	function deleteEdit(i){
+		var f =$(i).val();
+		console.log("deleteEmpのなか"+i);
+		console.log("deleteEmpのなかf"+f);
+
+
+		var rq = {rgp:f}//ここからサーブレットに渡す
+		console.log("var rqは"+f);
+		'use strict';
+		$.ajax({
+			Type : 'GET',
+			url : '/Employee_info/Employee_info_edit_Servlet',//サーブレットを確認
+			dataType : 'json',
+			data : rq,
+
+			success : function(pw) {
+				console.log('deleteEmpのajax'+pw);
+
+			},
+			error : function(XMLHttpRequest, textStatus, errorThrown) {
+				// サーバーとの通信に失敗した時の処理
+				alert('データの通信に失敗しました__Employee_info.js');
+				console.log(errorThrown)
+			}
+		});
+	}//var deleteEdit = functionの最後
+
+
+
 
 
 
@@ -113,46 +139,6 @@ $(document).ready(function () {
 });
 
 
-//---------------ここから削除機能---------------
-//var deleteEmp = function(){
-//	'use strict';
-//	cconsole.log('aaaa');
-//
-//
-//	$.ajax({
-//		Type : 'GET',
-//		url : '/Employee_info/DatabaseTest5',//サーブレットを確認
-//		dataType : 'json',
-//		data : requestQuery,
-//		success : function(pw) {
-//			console.log(pw);
-//
-//			for(var i = 0;i<pw.length; i++){
-//				var pwemp = pw[i];
-//				var edit ='<td><button type="button"   >編集</button></td>'
-//				var del= ' <td><button type="button" class = "js-delete-button" >削除</button></td>'
-//				$('#empTable').append('<tr>'+'<td>'+pwemp.empId+'</td>'+'<td>'+pwemp.empName+'</td>'+edit +del+'</tr>');
-//				}
-//
-//		},
-//		error : function(XMLHttpRequest, textStatus, errorThrown) {
-//			// サーバーとの通信に失敗した時の処理
-//			alert('データの通信に失敗しました');
-//			console.log(errorThrown)
-//		}
-//	});
-//}//var deleteEmp = functionの最後
-//
-//$(document).ready(function () {
-//	'use strict';
-//
-//	$('.js-delete-button').click(deleteEmp);
-//});
-//
-//
-//
-//
-//
-//
-//
-//
+
+
+
---
+++ src/appEmployee/Employee_info_edit_Servlet.java
@@ -0,0 +1,127 @@
+package appEmployee;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+@WebServlet("/Employee_info_edit_Servlet")
+public class Employee_info_edit_Servlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException{
+    	String testNo = request.getParameter("rgp");
+    	//String testNo = "3";
+    	//String empnum = request.getParameter("pwemp.empId");
+    	//System.out.println(empnum);
+    	System.out.println("リクエストパラメータ::"+testNo);
+     	//String c = changeId(testNo);
+    	//System.out.println(c);
+
+
+    	response.setContentType("text/html; charset=Shift_JIS");
+    	PrintWriter out = response.getWriter();
+        List <emp> empList =  new ArrayList<>();
+
+        String a = request.getParameter("rgp");
+        System.out.println("jsの値"+a);
+
+
+        //System.out.println(empList.get(a));
+
+
+        Connection con = null;
+        String url = "jdbc:oracle:thin:@localhost:1521:XE";
+		String user = "webapp";
+		String pass = "webapp";
+
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+             con = DriverManager.getConnection(url, user, pass);
+
+            Statement stmt = con.createStatement();
+
+           //String sql_delete ="delete from EMP_INFO \n where  EMPID = "+ c  ;
+           String sql_delete ="delete from EMP_INFO where EMPID ='"+testNo+"'" ;//ここを変数にする。
+
+            //String sql_delete ="delete from EMP_INFO ";
+
+
+             int i = stmt.executeUpdate(sql_delete);
+             System.out.println("削除したのは"+i+"行");//削除した行数
+
+                      String sql = "select \n" +
+				                    "ef.EMPID ee \n" +
+				    				",ef.NAME en \n" +
+				    				"from \n" +
+				    				"DEPART_KBN dk \n" +
+				    				",EMP_INFO ef \n" +
+				    				"where 1=1 \n" +
+				    				"and dk.DEPARTID = ef.DEPARTID \n" +
+				    				"order by  \n" +
+				    				"ef.EMPID \n" +
+				    				" \n";
+
+//
+//                      ResultSet rs1 = stmt.executeQuery(sql);
+//
+//
+//
+//            while (rs1.next()) {
+//				emp e1 = new emp();
+//				System.out.println("while文のなかDatabase5"+e1);
+//				e1.setEmpId(rs1.getString("ee"));
+//				e1.setEmpName(rs1.getString("en"));
+//				empList.add(e1);
+//				System.out.println(e1.getEmpId());
+//				System.out.println(e1.getEmpName());
+//            }
+//
+//            rs1.close();
+//            stmt.close();
+
+        }catch (ClassNotFoundException e){
+            out.println("ClassNotFoundException:" + e.getMessage());
+        }catch (SQLException e){
+            out.println("SQLException!!:" + e.getMessage());
+        }catch (Exception e){
+            out.println("Exception:" + e.getMessage());
+        }finally{
+            try{
+                if (con != null){
+                    con.close();
+                }
+            }catch (SQLException e){
+                out.println("SQLException:" + e.getMessage());
+            }
+        }
+        out.append(new ObjectMapper().writeValueAsString(empList));
+        System.out.println("最後"+empList);
+    }
+
+    public static String changeId(String x){
+    	int b = Integer.valueOf(x);
+    	int a =   +1;
+    	return "EMPID000"+ a;
+    }
+
+
+
+
+
+}
+
